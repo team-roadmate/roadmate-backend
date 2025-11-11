9e576a96d5a51a1b8d705ab0ab0ce884a1d579b4
YongTang <delicious50up55@gmail.com>
2025-11-12 01:26:37 +0900

    Initial commit

---
+++ src/main/java/com/trm/roadmate_backend/RoadmateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.trm.roadmate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadmateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadmateBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trm/roadmate_backend/RoadmateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trm.roadmate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadmateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
